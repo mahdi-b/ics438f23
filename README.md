ba79b009a3e894f5a66a272fdbf70a2ed8fb33c7
mahdi-b <mahdi-b@users.noreply.github.com>
2023-11-03 08:25:14 +0000

    published by GitHub Actions

---
+++ README.md
@@ -0,0 +1,6 @@
+# Morea Template
+
+This repository contains a template Morea course site.
+
+For documentation on how to use Morea, please see https://morea-framework.github.io.
+
---
+++ schedule/schedule-info.js
@@ -0,0 +1,7 @@
+moreaEventData = [
+  {title: "Introduction Big Data Analytics", url: "/ics438f23/modules/introduction", start: "2022-08-22", end: "2022-08-26T23:00"},
+  {title: "Using and Interacting with LLMs", url: "/ics438f23/modules/using-llms", start: "2023-10-24", end: "2023-11-02"},
+  {title: "Big Data Formats", url: "/ics438f23/modules/formats", start: "2022-08-29", end: "2022-09-05T23:00"},
+  {title: "Text Feature Engineering", url: "/ics438f23/modules/feature-engineering", start: "2023-10-03", end: "2023-10-10"},
+  {title: "Distributed Computing and Spark", url: "/ics438f23/modules/map-reduce", start: "2023-09-12", end: "2023-09-28"}
+]
